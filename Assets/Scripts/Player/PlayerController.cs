1dc2f56029be2bc6c21c9f2c710db052d59bf0ef
Eric Cook <jcook83@gatech.edu>
2015-01-31 22:37:48 -0500

    Beginnings of game states
    Put in beginner game manager that handles pausing
    
    Things can't be named "Rigidbody ..." apparently so changed that class
    to VelocityInfo.cs
    
    GameManager prefab
    
    Crude switch statement for PlayerController. Need to format everything
    else to use GameManager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Assets.Scripts;
+using Assets.Scripts.Data;
+using Assets.Scripts.Enums;
+using Assets.Scripts.Util;
 
 /*
  * Controls te player's movement
@@ -45,90 +47,100 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	void Update () {
-		//if our cheat is on, out collider is off, so we need to tun it back on
-		if(_colSwitch)
+		switch(GameManager._instance.State)
 		{
-			_colSwitch = false;
-			_playerCollider.enabled = true;
+		case GameState.Game:
+			//if our cheat is on, out collider is off, so we need to tun it back on
+			if(_colSwitch)
+			{
+				_colSwitch = false;
+				_playerCollider.enabled = true;
+			}
+
+			//if jump is pressed
+			if(CustomInput.JumpFreshPress && !CustomInput.Down)
+			{
+				_jump = true;
+			}
+
+			//if attack is pressed
+			if(CustomInput.AttackFreshPress)
+			{
+				_attack.gameObject.SetActive(true);
+			}
+
+			//if down is pressed
+			if(CustomInput.Down && CustomInput.JumpFreshPress)
+			{
+				//we should be ghosting
+				_ghost = true;
+				_ghostTimer = 0f;
+
+				_colSwitch = true;
+				_playerCollider.enabled = false;
+			}
+			//if ghosting
+			if(_ghost) _ghostTimer += Time.deltaTime;
+
+			//if ghosting is over
+			if(_ghostTimer > _ghostDelay)
+			{
+				_ghost = false;
+				_ghostTimer = 0;
+			}
+
+			//ignore collision between the player and platforms
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("player"), LayerMask.NameToLayer("platform"), _ghost);
+			break;
 		}
-
-		//if jump is pressed
-		if(CustomInput.JumpFreshPress && !CustomInput.Down)
-		{
-			_jump = true;
-		}
-
-		//if attack is pressed
-		if(CustomInput.AttackFreshPress)
-		{
-			_attack.gameObject.SetActive(true);
-		}
-
-		//if down is pressed
-		if(CustomInput.Down && CustomInput.JumpFreshPress)
-		{
-			//we should be ghosting
-			_ghost = true;
-			_ghostTimer = 0f;
-
-			_colSwitch = true;
-			_playerCollider.enabled = false;
-		}
-		//if ghosting
-		if(_ghost) _ghostTimer += Time.deltaTime;
-
-		//if ghosting is over
-		if(_ghostTimer > _ghostDelay)
-		{
-			_ghost = false;
-			_ghostTimer = 0;
-		}
-
-		//ignore collision between the player and platforms
-		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("player"), LayerMask.NameToLayer("platform"), _ghost);
 	}
 
 	//fixed update runs on a timed cycle (for physics stuff)
 	void FixedUpdate ()
 	{
-		//caching the horizontal input
-		float _h;
-
-		//assign value based on left or right input
-		if(CustomInput.Left) _h = -1f;
-		else if(CustomInput.Right) _h = 1f;
-		else _h = 0f;
-
-
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(_h * rigidbody2D.velocity.x < _maxSpeed)
+		switch(GameManager._instance.State)
 		{
-			//account for air control
-			if(_jumper.CanJump()) rigidbody2D.AddForce(Vector2.right * _h * _moveForce);
-			else rigidbody2D.AddForce(Vector2.right * _h * _moveForce * _airControl);
-		}
-		
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > _maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * _maxSpeed, rigidbody2D.velocity.y);
+		case GameState.Game:
+			//caching the horizontal input
+			float _h;
 
-		//flippng based on direction and movement
-		if(_h > 0 && !_facingRight)
-			Flip();
-		else if(_h < 0 && _facingRight)
-			Flip();
+			//assign value based on left or right input
+			if(CustomInput.Left) _h = -1f;
+			else if(CustomInput.Right) _h = 1f;
+			else _h = 0f;
 
-		//if jump was pressed
-		if(_jump)
-		{
-			//and player can jump
-			if(_jumper.CanJump())
+
+			// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+			if(_h * rigidbody2D.velocity.x < _maxSpeed)
 			{
-				//add upward force
-				rigidbody2D.AddForce(new Vector2(0f, _jumper.Force), ForceMode2D.Impulse);
+				//account for air control
+				if(_jumper.CanJump()) rigidbody2D.AddForce(Vector2.right * _h * _moveForce);
+				else rigidbody2D.AddForce(Vector2.right * _h * _moveForce * _airControl);
 			}
-			_jump = false;
+			
+			// If the player's horizontal velocity is greater than the maxSpeed...
+			if(Mathf.Abs(rigidbody2D.velocity.x) > _maxSpeed)
+				// ... set the player's velocity to the maxSpeed in the x axis.
+				rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * _maxSpeed, rigidbody2D.velocity.y);
+
+			//flippng based on direction and movement
+			if(_h > 0 && !_facingRight)
+				Flip();
+			else if(_h < 0 && _facingRight)
+				Flip();
+
+			//if jump was pressed
+			if(_jump)
+			{
+				//and player can jump
+				if(_jumper.CanJump())
+				{
+					//add upward force
+					rigidbody2D.AddForce(new Vector2(0f, _jumper.Force), ForceMode2D.Impulse);
+				}
+				_jump = false;
+			}
+			break;
 		}
 
 	}
