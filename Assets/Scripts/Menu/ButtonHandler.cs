68bc8ea53a9fcd5e021c580237028b36143ebe16
Jonathan Hunter <hunterjonathant@gmail.com>
2015-02-24 06:58:15 -0500

    Menu Scripts + main menu

---
+++ Assets/Scripts/Menu/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Menu
+{
+    abstract class ButtonHandler : MonoBehaviour
+    {
+        protected bool isLeft;
+        public abstract void wake();
+        public abstract void sleep();
+
+        public void setLeft()
+        {
+            isLeft = true;
+        }
+
+        public void setRight()
+        {
+            isLeft = false;
+        }
+    }
+}
